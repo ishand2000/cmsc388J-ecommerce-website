c341d382c2a964214744b38348e1818073c53f0a
ishand2000 <ishand2000@gmail.com>
2021-05-10 20:54:00 -0400

    uncommented mongodb settings

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,7 +1,7 @@
 from mongoengine import connect
 #DEFAULT_CONNECTION_NAME = connect('final_project')
 
-#MONGODB_SETTINGS = {'db':'final_project', 'alias':'default'}
+MONGODB_SETTINGS = {'db':'final_project', 'alias':'default'}
 
 # Stores all configuration values
 SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
